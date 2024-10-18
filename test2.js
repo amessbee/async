3a2fab11ba4f4a1c4a52950f65ca00a9fe486d91
Mudassir Shabbir <mudassir@agoric.com>
2024-10-18 07:02:17 +0500

    feat: two functions in Far object

--- test2.js
+++ test2.js
@@ -10,12 +10,18 @@ const remotePresence = Far('RemoteService', {
     sayHello: () => {
       return 'Hello from the other side';
     },
+    sayCustomGreetings: (name) => {
+      return `Hello ${name} from the other side`;
+    },
 
   });
-
 
 E(remotePresence).sayHello()
 .then(result => {
   console.log(result); // Expected output: 'Hello from the other side'
 } );
 
+E(remotePresence).sayCustomGreetings('devs')
+.then(result => {
+  console.log(result); // Expected output: 'Hello from the other side'
+} );
